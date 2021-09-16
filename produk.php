2d2eb5ac3e111a51a91d81c7bf2395a1fd24f8e6
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2021-09-16 12:15:54 +0700

    mencoba method berdasar pada video 3

--- produk.php
+++ produk.php
@@ -5,6 +5,14 @@ class Produk {
             $penulis =  "penulis",
             $penerbit = "penerbit",
             $harga = 0;
+    
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
+    // public function sayHello(){
+    //     return "Hello World";
+    // }
 }
 
 // $produk1 = new produk();
@@ -22,4 +30,14 @@ $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
+
+// echo $produk3->sayHello();
